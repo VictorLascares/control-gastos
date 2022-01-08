1ef46d04c389a5e1b5c5d0024a61c2613e84b026
Victor Lascares <vmlascares@gmail.com>
2022-01-08 15:56:16 -0600

    Formatear cantidad a formato de dinero

--- src/components/BudgetControl.jsx
+++ src/components/BudgetControl.jsx
@@ -1,11 +1,26 @@
 
 const BudgetControl = ({budget}) => {
+
+
+    const formatQuantity = (quantity) => {
+        return quantity.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        })
+    }
+
     return (
         <div className="contenedor-presupuesto contenedor sombra dos-columnas">
             <div>Grafica</div>
             <div className="contenido-presupuesto">
                 <p>
-                    <span>Presupuesto: </span> ${budget}
+                    <span>Presupuesto: </span>{formatQuantity(budget)}
+                </p>
+                <p>
+                    <span>Disponible: </span>{formatQuantity(0)}
+                </p>
+                <p>
+                    <span>Gastado: </span>{formatQuantity(0)}
                 </p>
             </div>
         </div>
